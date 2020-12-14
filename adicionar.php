0957be5656dcb4f13e12acfab4b4b66d00887124
vinicius1919 <75975361+vinicius1919@users.noreply.github.com>
2020-12-14 05:56:32 -0300

    Add files via upload

---
+++ adicionar.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Início</title>        
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+  <body>        
+    <div class="container">
+      <div class="row">
+        <div class="col" align="center">
+          <img src="imagens/logosanp.jpg" alt="some text" width=200 height=200> 
+        </div>      
+      </div>        
+  </div><!-- fim Div container -->
+  <div class="col-12" align="center">
+    <?php echo "Adicionar Tarefa"; ?>
+  </div>
+  <div class="row">
+  <div class="col-4"> </div> 
+    <div class="col-4">    
+        <input type="text" class="form-control" placeholder="Descrição" aria-describedby="basic-addon1">  
+    </div>
+    <div class="col-4"></div>
+  </div> <br>
+  <div class="col-12" align="center">
+    <button type="button" class="btn btn-secondary btn-lg">Adicionar</button>
+  </div><br>  
+  <div class="col-12" align="center">
+    <a href="index.php"><button type="button" class="btn btn-primary btn-sm">Inicio</button></a>
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,85 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Início</title>        
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+	<body>        
+    <div class="container">
+	  <div class="row">
+	    <div class="col" align="center">
+	      <img ali src="imagens/logosanp.jpg" alt="some text" width=200 height=200> 
+	    </div>
+	  </div>	      
+	</div><!-- fim Div container -->
+    <br>
+    <div class="container">
+	  	<div class="row">
+		  	<div class="col-4">	    	
+		    </div>
+		    <div class="col-4">
+		    	<?php echo "Tarefas"; ?>
+		    </div>
+		    <div class="col-4">
+		      <a href="adicionar.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Adicionar Tarefa</a>
+		    </div>
+	  	</div>
+	  <br>
+	    <div class="row">
+		    <div class="col">		     
+		    </div>
+		    <div class="col-6">
+		    	<?php echo "Tarefa 1"; ?>
+		    </div>
+		    <div class="col">
+		    	<button type="button" class="btn btn-primary"> Editar</button>
+		    	<button type="button" class="btn btn-success">Finalizar</button>
+		    	<button type="button" class="btn btn-danger">Deletar</button>
+		    </div>
+	  	</div><!-- fim div class row --><br>
+	  	<div class="row">
+		    <div class="col">		     
+		    </div>
+		    <div class="col-6">
+		    	<?php echo "Tarefa 2"; ?>
+		    </div>
+		    <div class="col">
+		    	<button type="button" class="btn btn-primary"> Editar</button>
+		    	<button type="button" class="btn btn-success">Finalizar</button>
+		    	<button type="button" class="btn btn-danger">Deletar</button>
+		    </div>
+	  	</div><!-- fim div class row --><br>
+	  	<div class="row">
+		    <div class="col">		     
+		    </div>
+		    <div class="col-6">
+		    	<?php echo "Tarefa 3"; ?>
+		    </div>
+		    <div class="col">
+		    	<button type="button" class="btn btn-primary"> Editar</button>
+		    	<button type="button" class="btn btn-success">Finalizar</button>
+		    	<button type="button" class="btn btn-danger">Deletar</button>
+		    </div>
+	  	</div><!-- fim div class row --><br>
+	  	<div class="row">
+		    <div class="col">		     
+		    </div>
+		    <div class="col-6">
+		    	<?php echo "Tarefa 4"; ?>
+		    </div>
+		    <div class="col">
+		    	<button type="button" class="btn btn-primary"> Editar</button>
+		    	<button type="button" class="btn btn-success">Finalizar</button>
+		    	<button type="button" class="btn btn-danger">Deletar</button>
+		    </div>
+	  	</div><!-- fim div class row --><br>
+	</div><!-- fim Div container -->	
+    </body>
+    <br>
+    <br>
+    <div class="col-sm-12" align="center">
+    	<?php echo "<small> Vinicius Sousa Rodrigues 2020</small>"; ?>    	
+    </div>
+
+</html>
